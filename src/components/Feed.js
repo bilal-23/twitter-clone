f25e328387a3865ac064beee9d5eecdf5e86c1f0
bilal-23 <mansuribilal101@gmail.com>
2021-07-11 19:04:35 +0530

    added posts

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -2,12 +2,16 @@ import React from 'react'
 import './Feed.scss'
 import FeedHeader from './FeedHeader'
 import Tweetbox from './Tweetbox'
+import Post from './Post'
 
 const Feed = () => {
     return (
         <div className="feed">
             <FeedHeader />
             <Tweetbox />
+            <Post />
+            <Post />
+            <Post />
         </div>
     )
 }
---
+++ src/components/Post.js
@@ -0,0 +1,42 @@
+import { Avatar } from '@material-ui/core'
+import { VerifiedUserRounded, ChatBubbleOutline, FavoriteBorder, Publish, Repeat } from '@material-ui/icons'
+import React from 'react'
+import "./Post.scss"
+
+const Post = ({ displayName, userName, verified, timestamp, text, image, avatar }) => {
+    return (
+        <div className="post">
+            <div className="post__avatar">
+                <Avatar src="https://avatars.githubusercontent.com/u/71442068?v=4" />
+            </div>
+            <div className="post__body">
+                <div className="post__header">
+                    <div className="post__headerText">
+                        <h3>Bilal Mansuri
+                            <span className="post__headerSpan">
+                                <VerifiedUserRounded className="post__badge" /> @bilal
+                            </span>
+                        </h3>
+                    </div>
+                    <div className="post__headerDescription">
+                        <p>Special thanks to
+                            @KevinJPowell
+                            , whose channel has wonderful videos on CSS.
+                            The tutorials on position property helped me a great deal in understanding the property, and completing this blog!</p>
+                    </div>
+                </div>
+                <div className="post__img">
+                    <img src="https://pbs.twimg.com/media/E6ANOyeUYAINlda?format=png&name=small" />
+                </div>
+                <div className="post__footer">
+                    <ChatBubbleOutline className="post__footer__icons icon icon__comment" />
+                    <Repeat className="post__footer__icons icon icon__retweet" />
+                    <FavoriteBorder className="post__footer__icons icon icon__like" />
+                    <Publish className="post__footer__icons icon icon__share" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Post
